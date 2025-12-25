2eb76b9051f10bb65da5599d924a6f11f927b410
Shojol Islam <shojolislam3231@gmail.com>
2025-12-25 22:53:33 +0600

    working on top bar lLogo

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const TopBar = () => {
   return (
-    <section className="relative w-full bg-[#3C3C3C] px-2 text-[13px] text-[#cccccc]">
+    <section className="relative min-h-6.5 w-full bg-[#3C3C3C] px-2 text-[13px] text-[#cccccc]">
       <Image src={"/logo.png"} width={40} height={20} alt="logo" />
 
       <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
