74d720cbd944a0c174b44724f09041f570b92972
Shojol Islam <shojolislam3231@gmail.com>
2026-02-26 21:52:37 +0600

    wrking on issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import TopBar from "@/components/TopBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
